2b6a6bbdf6a43d05330c9d597c3f92fa4f0d05b3
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2024-02-06 11:15:09 -0500

    Update version number to 0.1.5 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="terrain_tools",
-    version="0.1.4",
+    version="0.1.5",
     author="Ryan Hamilton",
     author_email="ryan.hamilton@ec.gc.ca",
     package_dir={"": "src"},
